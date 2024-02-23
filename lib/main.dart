39c0102c46477a1d3c1a6ea9954665f312480942
Mujahid_Sadiq <subhanmustafai1234@gmail.com>
2024-02-23 22:07:49 +0500

    day3 is in pending

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_learning_codepur/pages/login_pages.dart';
 import 'package:flutter_learning_codepur/pages/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,6 +16,8 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily,
+          primaryTextTheme: GoogleFonts.latoTextTheme(),
         ),
         routes: {
           // routing
--- lib/pages/login_pages.dart
+++ lib/pages/login_pages.dart
@@ -5,24 +5,79 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text(
-          'Login page ',
-          style: TextStyle(
-            fontSize: 40,
-            fontWeight: FontWeight.bold,
-            color: Colors.blue,
+    return SafeArea(
+      child: Material(
+          child: Column(
+        children: [
+          const SizedBox(
+            height: 10,
           ),
+          Image.asset(
+            'assets/images/undraw_secure_login_pdn4.png',
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          const Text(
+            'Welcome ',
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 16.0,
+              horizontal: 32.0,
+            ),
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(
+                      hintText: 'Enter username', labelText: 'Username'),
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                      hintText: 'Enter password', labelText: 'password'),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
 
-          // textScaler: TextScaler.linear(), // Adjust the scale factor as needed
-        ),
-      ),
+                // elevated button
+
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.resolveWith<Color>(
+                      (Set<MaterialState> states) {
+                        if (states.contains(MaterialState.pressed)) {
+                          return Colors.blue.shade200; // Color when pressed
+                        }
+                        return Colors.blue; // Default color
+                      },
+                    ),
+                    // Other properties can be set here
+                  ),
+                  child: const Text(
+                    'login',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      )),
     );
   }
 }
-
-
 
 /*
 import 'package:flutter/material.dart';
