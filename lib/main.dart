23dff293a1ec3715611dba2e9049e6589f55932d
Mujahid_Sadiq <subhanmustafai1234@gmail.com>
2024-02-23 12:34:25 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Material(
+        child: Center(
+          child: Text(
+            'welocome to 30 days of flutter ',
+          ),
+        ),
+      ),
+    );
+  }
+}
